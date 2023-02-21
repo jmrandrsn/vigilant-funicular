21c0f37fd87f7dbc187a92907469f41a2c4c3730
Jhemarr Anderson <jhemarr@protonmail.com>
2023-02-21 14:48:04 -0500

    GEO API and AutoComplete

--- client/src/App.js
+++ client/src/App.js
@@ -1,44 +1,39 @@
-import './App.css';
 import { useState } from 'react';
 import { key, API_URL } from './utils/api';
-import SearchBar from './components/SearchBar';
 import WeatherCard from './components/WeatherCard';
+import SearchBar from './components/SearchBar';
 
 function App() {
 	const [city, setCity] = useState('');
 	const [weatherData, setWeatherData] = useState(null);
 
-	const fetchData = async (city) => {
-		const response = await fetch(
-			`${API_URL}${city}&appid=${key}&units=imperial`,
-		);
-		const data = await response.json();
-		// console.log(data);
-		const lat = data.coord.lat;
-		const lon = data.coord.lon;
+	const fetchData = async (option) => {
+		const lat = await option.coords.lat;
+		const lon = await option.coords.lon;
+		// const city = await option.city;
+		console.log(lon);
+		console.log(lat);
+
 		const forecastResponse = await fetch(
-			`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${key}&units=imperial`,
+			`${API_URL}&lat=${lat}&lon=${lon}&appid=${key}&units=imperial`,
 		);
 		const forecastData = await forecastResponse.json();
+
 		console.log(forecastData);
-		setWeatherData(forecastData);
+		// setCity(option.city);
+
+		// setWeatherData(forecastData);
 	};
 
 	return (
 		<div className="App">
 			<header className="App-header">
 				<h1 className="title">Vigilant Funicular</h1>
-				<SearchBar
-					className="searchBar"
-					onSearch={(value) => setCity(value)}
-					onApi={fetchData}
-				/>
+				<SearchBar onSearch={(value) => setCity(value)} onApi={fetchData} />
 			</header>
 			<div className="weather_container">
-				<WeatherCard city={city} />
+				{city && <WeatherCard city={city} />}
 				{weatherData && <WeatherCard data={weatherData} />}
-				{/* //Instead of Passing the city name I want to
-			pass the entire API response here */}
 			</div>
 		</div>
 	);
